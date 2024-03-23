ab7d3a3a5a5d2a37979a451c2ef91536b1c6a446
sana-lazystar <sana.lazystar@gmail.com>
2024-03-24 01:21:01 +0900

    Add configuration for protected routes

--- app/middleware.ts
+++ app/middleware.ts
@@ -1,5 +1,6 @@
 export { default } from 'next-auth/middleware';
 
+// NOTE: Configuration for protected routes (without authentication)
 export const config = {
   // TODO: Make matchers to protected routes
   matcher: [],
